02f503c9238b7f3ce7e385bc6bea1a645e42ee0a
pmgesualdi <pmgesualdi@gmail.com>
2017-11-21 11:58:11 -0300

    cssToggle function

--- js/agency.js
+++ js/agency.js
@@ -1,28 +1,19 @@
 // Agency Theme JavaScript
 $( document ).ready(function() {
-    
-    $('.team-member img').toggleClass(function() {
-        return ('ontouchstart' in window) ? 'thumbnail-mobile' : 'thumbnail';
-        }
-    );
-    $('.fa-wrench').toggleClass(function() {
-        return ('ontouchstart' in window) ? 'rotate-mobile' : 'rotate';
-      }
-    );
-    $('.fa-laptop').toggleClass(function() {
-            return ('ontouchstart' in window) ? 'shrink-mobile' : 'shrink';
-        }
-    );
-    $('.fa-lock').toggleClass(function() {
-        return ('ontouchstart' in window) ? 'shake-mobile' : 'shake';
-      }
-    );
+    $('.team-member img').toggleClass(getCss('thumbnail'));
+    $('.fa-wrench').toggleClass(getCss('rotate'));
+    $('.fa-laptop').toggleClass(getCss('shrink'));
+    $('.fa-lock').toggleClass(getCss('shake'));
 
     sr.reveal($('#services'), { mobile: true, duration: 799, delay: 1 });
     sr.reveal($('#about'), { mobile: true, duration: 799, delay: 1 });
     sr.reveal($('#footer'), { mobile: true, duration: 799, delay: 1 });
 
+    function getCss (name) {
+        return ('ontouchstart' in window) ? name + '-mobile' : name;
+    }
 });
+
 
 (function($) {
     "use strict"; // Start of use strict
