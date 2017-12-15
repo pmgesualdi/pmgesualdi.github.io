cca43f1e1327cbaf8081c8208aa9920a3d561c9c
pmgesualdi <pmgesualdi@gmail.com>
2017-12-15 12:08:53 -0300

    git carousel logic refactor

--- js/blueberry.js
+++ js/blueberry.js
@@ -18,39 +18,46 @@ $( document ).ready(function() {
         dataType: 'jsonp',
         success: function(response) {
             //Get MercadoPago promos
-            response[2].forEach(function(obj) {
-                promosMP.push([obj.issuer.id, obj.issuer.name, obj.expiration_date, obj.max_installments]);
-            });
+            response[2].forEach(function(record) {
+                var obj = {};
+                
+                obj["id"] = record.issuer.id;
+                obj["name"] = record.issuer.name,
+                // obj["expiration_date"] = formatDate(record.expiration_date),
+                obj["max_installments"] = record.max_installments;
+
+                promosMP.push(obj);
+            });          
 
             $.getJSON('./resources/banks.json', function(data){
                 infoBancos = data; //Get bank logos
-                initCarousel(); //Render Carousel
+                renderCarousel(); //Render Carousel
             })
         }
-     })
+    })
     
     function getCss (name) {
         return ('ontouchstart' in window) ? name + '-mobile' : name;
     }
     
-    function initCarousel () {
-        // for(var i = 1; i < promos.length - 1; i++) {
-        //     var imgUrl;
-        //     var currentPromo = promos[i];
-        //     var promoDate = getDate(currentPromo.expiration_date);
+    function renderCarousel () {
+        for(var i = 1; i < promosMP.length; i++) {
+            // var imgUrl;
+            var promo = promosMP[i];
             
-        //     if (currentPromo.payment_methods[1]) {
-        //         imgUrl = currentPromo.payment_methods[1].thumbnail;
-        //     } else {
-        //         imgUrl = currentPromo.payment_methods[0].thumbnail;
-        //     }
-            
-        //     $('<div><div><img src="' + imgUrl + '">'
-        //         + '<div>Hasta ' + currentPromo.max_installments + ' cuotas sin interés!</div>'
-        //         + '<div>' + promoDate + '</div></div>'
-        //     ).appendTo('.carousel');
-        // }
+            if (infoBancos.hasOwnProperty(promo.id)) {
+                $('<div><img src=' + infoBancos[promo.id].url + ' alt=' + promo.name + ' />'
+                    + '<div>Hasta ' + promo.max_installments + ' cuotas sin interés!</div>'
+                    + '<div>Válido hasta: ' + promo.expiration_date + '</div></div>'
+                ).appendTo('.carousel');
+            }
+        }
+
+        initCarousel();
         
+    }
+    
+    function initCarousel () {
         var slides = 'ontouchstart' in window ? 1 : 3;
         $('.carousel').slick({
             infinite: true,
@@ -58,10 +65,4 @@ $( document ).ready(function() {
             slidesToScroll: slides
         });
     }
-
-    // function getDate (date) {
-    //     // var formateDate = Date(date);
-        
-    //     // return formateDate.getDa
-    // }
 });
