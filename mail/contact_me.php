e47a3c09aa5e047506f1dd4a7b2a527229d094d1
pmgesualdi <pmgesualdi@gmail.com>
2017-12-30 11:43:47 -0300

    contact form message language

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,10 +17,10 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
 $to = 'consulting@blueberrygroup.com.ar'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@blueberrygroup.com.ar\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$email_subject = "Formulario de contacto web de:  $name";
+$email_body = "Has recibido un nuevo mensaje desde el formulario de contacto de tu página web.\n\n"."Estos son los detalles:\n\nNombre: $name\n\nEmail: $email_address\n\nTeléfono: $phone\n\nMensaje:\n$message";
+$headers = "De: noreply@blueberrygroup.com.ar\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Respuesta: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
