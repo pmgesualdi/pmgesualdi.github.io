06936616acfe03277586ddd18d4101e0905dde88
pmgesualdi <pmgesualdi@gmail.com>
2017-12-30 11:56:21 -0300

    fix #4

--- js/contact_me.js
+++ js/contact_me.js
@@ -35,7 +35,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Su mensaje ha sido enviado con \u00E9ito!. </strong>");
+            .append("<strong>Su mensaje ha sido enviado con \u00E9xito!. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
